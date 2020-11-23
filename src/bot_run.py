1cfa4e28814ae649dfa731a1568abcea6677a109
alexnurin <an362@mail.ru>
2020-11-23 09:36:44 +0300

    telebot: base text and help message requirements (telebot)

---
+++ src/bot_run.py
@@ -0,0 +1,33 @@
+import telebot
+import secret
+
+telegram_token = secret.token
+
+bot = telebot.TeleBot(telegram_token)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def help_message(message):
+    bot.send_message(message.chat.id, '''
+/help - все команды
+''')
+
+
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+    text_reversed = message.text[::-1]
+    bot.send_message(message.chat.id, text_reversed)
+
+
+@bot.message_handler(lambda message: True)
+def another_message(message):
+    help_message(message)
+
+
+if __name__ == '__main__':
+    print('Start!\n')
+    while 1:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as exception:
+            print(exception)
