6400941c2da0556907f67d09d04ccf4e02d7667c
alexnurin <an362@mail.ru>
2020-11-29 20:40:40 +0300

    logging bugfix

--- src/bot_logging.py
+++ src/bot_logging.py
@@ -7,4 +7,4 @@ def save_message(message):
     if PRINT:
         print(message)
     with open('../data/saved.txt', 'a') as f:
-        f.write(f'{message}')
+        f.write(f'{message.chat.id}: {message.text}\n')
