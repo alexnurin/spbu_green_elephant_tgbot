2f764c4ccd510799cbd82be7e72212fb998bb3c7
Maxon081102 <nikitinm117@gmail.com>
2020-12-11 17:45:30 +0300

    Функция для бота и исправленная программа для базы данных

---
+++ Data/Func_data.py
@@ -0,0 +1,29 @@
+
+#Functions for students
+#1)
+'''удаляет телеграмм из базы студентов(принимает имя студента,как в таблице)
+def delete_telega_in_data(students_name):
+    with sq.connect("students_spbu_real.db") as con:
+        cur = con.cursor()
+        cur.execute(f"""UPDATE students_mkn SET telega_id = '{0}' WHERE name= '{students_name}' """)
+'''
+#2)
+'''получает номер группы и возвращает массив с айдишниками телеграммов 
+def get_telega_from_data(group):
+    all_id_from_group = []
+    for elem in group:
+        with sq.connect("students_spbu_real.db") as con:
+            cur = con.cursor()
+            cur.execute(f"""SELECT telega_id FROM students_mkn WHERE student_group='{elem}' """)
+            results = cur.fetchall()
+            print(results)
+            for i in range(len(results)):
+                if results[i] != (0,):
+                    all_id_from_group.append(results[i][0])
+    return all_id_from_group'''
+#3)
+'''Получает имя студента ,как в таблице , и его айдишник .Добавляет телеграмм студента в базу студнтов 
+def add_telega_in_data(students_name, his_id):
+    with sq.connect("students_spbu_real.db") as con:
+        cur = con.cursor()
+        cur.execute(f"""UPDATE students_mkn SET telega_id = '{his_id}' WHERE name= '{students_name}' """)'''
