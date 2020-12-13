7b1cd48e9c75ce3a5634b71bec3581dde4fa0c1f
Maxon081102 <nikitinm117@gmail.com>
2020-12-13 08:50:39 +0300

    Добавил пароли для преподавателей

--- Data/Func_data_s.py
+++ Data/Func_data_s.py
@@ -8,22 +8,34 @@ def delete_telega_in_data(students_name):
         cur.execute(f"""UPDATE students_mkn SET telega_id = '{0}' WHERE name= '{students_name}' """)
 '''
 #2)
-'''получает номер группы и возвращает массив с айдишниками телеграммов 
-def get_telega_from_data(group):
+#получает номер группы и возвращает массив с айдишниками телеграммов
+'''def get_telega_from_data(group):
     all_id_from_group = []
     for elem in group:
         with sq.connect("students_spbu_real.db") as con:
             cur = con.cursor()
             cur.execute(f"""SELECT telega_id FROM students_mkn WHERE student_group='{elem}' """)
             results = cur.fetchall()
-            print(results)
             for i in range(len(results)):
-                if results[i] != (0,):
+                if results[i] != ('0',):
                     all_id_from_group.append(results[i][0])
-    return all_id_from_group'''
+    return all_id_from_group
 #3)
-'''Получает имя студента ,как в таблице , и его айдишник .Добавляет телеграмм студента в базу студнтов 
+#Получает имя студента ,как в таблице , и его айдишник .Добавляет телеграмм студента в базу студнтов
+import sqlite3 as sq
+name =input()
+id=input()
+
 def add_telega_in_data(students_name, his_id):
     with sq.connect("students_spbu_real.db") as con:
         cur = con.cursor()
-        cur.execute(f"""UPDATE students_mkn SET telega_id = '{his_id}' WHERE name= '{students_name}' """)'''
+        cur.execute(f"""UPDATE students_mkn SET telega_id = '{his_id}' WHERE name= '{students_name}' """)
+add_telega_in_data(name,id)
+name =input()
+id=input()
+add_telega_in_data(name,id)
+name =input()
+id=input()
+add_telega_in_data(name,id)
+group=[1]
+print(get_telega_from_data(group))'''
