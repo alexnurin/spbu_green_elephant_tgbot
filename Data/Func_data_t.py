b2fd805602b9a3989d00406abbdeb934c663422f
Maxon081102 <nikitinm117@gmail.com>
2020-12-15 11:10:07 +0300

    Функция для учителей теперь работает по паролю

--- Data/Func_data_t.py
+++ Data/Func_data_t.py
@@ -1,6 +1,9 @@
 #1)добавляем имя преподавателя в базу
-'''
-def add_telega_in_data_t(prepod, his_id):
+import sqlite3 as sq
+password=input()
+id =input()
+def add_telega_in_data_t(password, his_id):
     with sq.connect("teachers.db") as con:
         cur = con.cursor()
-        cur.execute(f"""UPDATE teachers_mkn_1 SET teachers_authentication = '{his_id}' WHERE name= '{prepod}' """)'''
+        cur.execute(f"""UPDATE teachers_mkn_1 SET teachers_authentication = '{his_id}' WHERE password_t= '{password}' """)
+add_telega_in_data_t(password,id)
