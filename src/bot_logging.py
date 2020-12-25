a4d40548309274f5a95409b122cb2d6cfae98bd3
anyfree8 <71082717+anyfree8@users.noreply.github.com>
2020-12-25 20:57:41 +0300

    Delete bot_logging.py
    ты мне больше не друг

--- src/bot_logging.py
+++
@@ -1,9 +0,0 @@
-
-PRINT = 1
-
-
-def save_message(message):
-    if PRINT:
-        print(message)
-    with open('../data/saved.txt', 'a') as f:
-        f.write(f'{message.chat.id}: {message.text}\n')
