e825ca06ecef05581ef2ddd3dff0a202007b16c7
Maxon081102 <64801664+Maxon081102@users.noreply.github.com>
2020-12-26 18:32:04 +0300

    Delete Func_data_t.py
    добавил случайно

--- Data/Func_data_t.py
+++
@@ -1,9 +0,0 @@
-#1)добавляем имя преподавателя в базу
-import sqlite3 as sq
-password=input()
-id =input()
-def add_telega_in_data_t(password, his_id):
-    with sq.connect("teachers.db") as con:
-        cur = con.cursor()
-        cur.execute(f"""UPDATE teachers_mkn_1 SET teachers_authentication = '{his_id}' WHERE password_t= '{password}' """)
-add_telega_in_data_t(password,id)
