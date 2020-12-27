9eab4993390ddae885dcd4aad80416b04a984fce
cuckold <Anufriev0802@mail.ru>
2020-12-27 21:25:42 +0300

    logging added

--- src/Func_data_s.py
+++ src/Func_data_s.py
@@ -1,30 +1,61 @@
-import sqlite3 as sq
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from src.bot_logging import *
+
+engine_students = create_engine('sqlite:///students_spbu_real (5).db', echo=True)
+engine_teachers = create_engine('sqlite:///teachers (2).db', echo=True)
+meta_students = MetaData(engine_students)
+meta_teachers = MetaData(engine_teachers)
+students = Table(
+    'students_mkn', meta_students,
+    Column('students_id', Integer, primary_key=True),
+    Column('name', String),
+    Column('student_group', Integer),
+    Column('english_group', Integer),
+    Column('telega_id', String, default=0)
+)
+
+teachers = Table(
+    'teachers_mkn_1', meta_teachers,
+    Column('teachers_authentication', Integer, default=0),
+    Column('name', String),
+    Column('teaching_group', Integer, default=1),
+    Column('subject', String),
+    Column('password_t', Integer, default=0)
+)
 
 
 # add student to "students_spbu_real (5).db"
 def register_student(students_name: str, student_id: int):
-    with sq.connect("students_spbu_real (5).db") as con:
-        cur = con.cursor()
-        cur.execute(f"""UPDATE students_mkn SET telega_id = '{student_id}' WHERE name= '{students_name}' """)
+    conn = engine_students.connect()
+    stmt = students.update().where(students.c.name == students_name).values(telega_id=str(student_id))
+    conn.execute(stmt)
+    f_data_logger.info('Зарегистрировался студент: ' + students_name + '. id: ' + str(student_id))
 
 
 # delete student from "students_spbu_real (5).db"
 def unregister_student(student_id: int):
-    with sq.connect("students_spbu_real (5).db") as con:
-        cur = con.cursor()
-        cur.execute(f"""UPDATE students_mkn SET telega_id = '{0}' WHERE telega_id= '{student_id}' """)
+    conn = engine_students.connect()
+    stmt = students.update().where(students.c.telega_id == str(student_id)).values(telega_id='0')
+    conn.execute(stmt)
+    f_data_logger.info('Отписался студент с id: ' + str(student_id))
 
 
 # Checking the uniqueness of the id of the student who wants to register
 def check_student_uniqueness(student_id: int) -> bool:
-    with sq.connect("students_spbu_real (5).db") as con:
-        cur = con.cursor()
-        cur.execute(f"""SELECT telega_id FROM students_mkn WHERE telega_id='{str(student_id)}' """)
-        results = cur.fetchall()
-        if results:
-            return False
-        else:
-            return True
+    results_empty = True
+    conn = engine_students.connect()
+    s = students.select().where(students.c.telega_id == str(student_id))
+    results = conn.execute(s)
+    # check for emptiness
+    for value in results:
+        results_empty = False
+        break
+    if results_empty:
+        f_data_logger.info('Студент c id: ' + str(student_id) + ' прошел проверку на уникальность')
+        return True
+    else:
+        f_data_logger.info('Попытка повторной регистрации c id: ' + str(student_id))
+        return False
 
 
 # gets a list of student ids from given groups
@@ -34,14 +65,14 @@ def get_students_id_list(groups: list) -> list:
     :return all_id_from_groups:  list with id for each student from input groups
     """
     all_id_from_groups = []
+    conn = engine_students.connect()
     for group in groups:
-        with sq.connect("students_spbu_real (5).db") as con:
-            cur = con.cursor()
-            cur.execute(f"""SELECT telega_id FROM students_mkn WHERE student_group='{group}' """)
-            results = cur.fetchall()
-            for i in range(len(results)):
-                if results[i] != ('0',):
-                    all_id_from_groups.append(results[i][0])
+        s = students.select().where(students.c.student_group == group)
+        results = conn.execute(s)
+        for value in results:
+            if value[4] != '0':
+                all_id_from_groups.append(value[4])
+    f_data_logger.info(f'id студентов из групп/группы {groups} были выбраны из базы данных')
     return all_id_from_groups
 
 
@@ -50,16 +81,20 @@ def register_teacher(user_id: int, password: str):
     """
     :return bool: True if user is teacher and False otherwise
     """
-    with sq.connect("teachers (2).db") as con:
-        cur = con.cursor()
-        cur.execute(f"""SELECT name FROM teachers_mkn_1 WHERE password_t= '{password}' """)
-        results = cur.fetchall()
-        if results:
-            cur.execute(
-                f"""UPDATE teachers_mkn_1 SET teachers_authentication = '{user_id}' WHERE password_t= '{password}' """)
-            return True
-        else:
-            return False
+    results_empty = True
+    conn = engine_teachers.connect()
+    s = teachers.select().where(teachers.c.password_t == password)
+    results = conn.execute(s)
+    for value in results:
+        results_empty = False
+    if not results_empty:
+        stmt = teachers.update().where(teachers.c.password_t == password).values(teachers_authentication=user_id)
+        conn.execute(stmt)
+        f_data_logger.info('Преподаватель  с id: ' + str(user_id) + ' зарегистрирован')
+        return True
+    else:
+        f_data_logger.info('Пароль ' + password + ' не найден')
+        return False
 
 
 # recognize teacher by id
@@ -67,13 +102,15 @@ def get_teacher_name(teacher_id: int):
     """
     :return: True and str name of teacher if teacher was register
     """
-    with sq.connect("teachers (2).db") as con:
-        cur = con.cursor()
-        cur.execute(f"""SELECT name FROM teachers_mkn_1 WHERE teachers_authentication='{teacher_id}' """)
-        results = cur.fetchall()
-        if results:
-            return True, results[0][0]
-        else:
-            return False, "developer"
-
-
+    teacher_name = "None"
+    conn = engine_teachers.connect()
+    s = teachers.select().where(teachers.c.teachers_authentication == teacher_id)
+    results = conn.execute(s)
+    for value in results:
+        teacher_name = value[1]
+    if teacher_name != "None":
+        f_data_logger.info('Найдено имя преподавателя: ' + teacher_name)
+        return True, teacher_name
+    else:
+        f_data_logger.info('Имя не найдено, id: ' + str(teacher_id))
+        return False, "developer"
